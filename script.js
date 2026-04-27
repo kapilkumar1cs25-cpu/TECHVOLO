8bcaa84759e32c1adc3d39b21e96d3ed1b2f0f27
kapilkumar1cs25-cpu <kapil.kumar1_cs25@gla.ac.in>
2026-04-27 19:19:03 +0530

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+// Kapil Kumar LinkedIn
+document.getElementById("kapil-btn").addEventListener("click", () => {
+  window.open("https://www.linkedin.com/in/kapil-kumar-12kdjaara", "_blank");
+});
+
+// Member 1 Instagram
+document.getElementById("member1-btn").addEventListener("click", () => {
+  window.open("https://www.instagram.com/kdjaara12/", "_blank");
+});
+
+// Member 2 GitHub
+document.getElementById("member2-btn").addEventListener("click", () => {
+  window.open("https://chat.whatsapp.com/CAYMgNk9VhTAgNwHxDUzSa", "_blank");
+});
